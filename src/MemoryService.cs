2251242e8cc03b0ac1d1f355bc4df3874b310232
권대선 <dsun@hanmail.net>
2025-12-24 16:39:06 +0900

    ConcurrentDictionary

--- src/MemoryService.cs
+++ src/MemoryService.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
-using System.Collections;
+using System.Collections.Concurrent;
 
 namespace MetaFrm.Service
 {
@@ -8,7 +8,7 @@ namespace MetaFrm.Service
     /// </summary>
     public class MemoryService : IService
     {
-        private readonly Hashtable Hashtable = [];
+        private readonly ConcurrentDictionary<string, string?> _memory = [];
 
         /// <summary>
         /// 생성자 입니다.
@@ -27,75 +27,67 @@ namespace MetaFrm.Service
                 if (serviceData.ServiceName == null || !serviceData.ServiceName.Equals("MetaFrm.Service.MemoryService"))
                     throw new Exception("Not MetaFrm.Service.MemoryService");
 
-                response = this.Excute(serviceData);
-            }
-            catch (MetaFrmException exception)
-            {
-                Factory.Logger.LogError(exception, "{Message}", exception.Message);
-                return new Response(exception);
+                response = this.Execute(serviceData);
             }
             catch (Exception exception)
             {
-                Factory.Logger.LogError(exception, "{Message}", exception.Message);
+                if (Factory.Logger.IsEnabled(LogLevel.Error))
+                    Factory.Logger.LogError(exception, "{Message}", exception.Message);
                 return new Response(exception);
             }
 
             return response;
         }
 
-        Response Excute(ServiceData serviceData)
+        private Response Execute(ServiceData serviceData)
         {
-            Response? response;
+            Response response;
 
-            response = new()
-            {
-                DataSet = new()
-            };
+            response = new();
 
-            try
+            foreach (string cmdKey in serviceData.Commands.Keys)
             {
-                foreach (string table in serviceData.Commands.Keys)
+                switch (serviceData.Commands[cmdKey].CommandText)
                 {
-                    switch (serviceData.Commands[table].CommandText)
-                    {
-                        case "Get":
-                            response.DataSet.DataTables.Add(new());
-                            response.DataSet.DataTables[0].DataColumns.Add(new("VALUE", "System.String"));
+                    case "Get":
+                        var table = new Data.DataTable();
+                        table.DataColumns.Add(new("VALUE", typeof(string).FullName!));
 
-                            for (int i = 0; i < serviceData[table].Values.Count; i++)
+                        response.DataSet ??= new();
+                        response.DataSet.DataTables.Add(table);
+
+                        for (int i = 0; i < serviceData[cmdKey].Values.Count; i++)
+                        {
+                            Data.DataRow dataRow = new();
+
+                            var key = serviceData[cmdKey].GetValue("KEY", i);
+                            if (key != null && key is string str)
                             {
-                                Data.DataRow dataRow1 = new();
-                                response.DataSet.DataTables[0].DataRows.Add(dataRow1);
-
-                                var key = serviceData[table].GetValue("KEY", i);
-                                if (key != null)
-                                    dataRow1.Values.Add("VALUE", new Data.DataValue(this.Hashtable[key]));
-                                else
-                                    dataRow1.Values.Add("VALUE", new Data.DataValue(null));
+                                this._memory.TryGetValue(str, out var value1);
+                                dataRow.Values.Add("VALUE", new Data.DataValue(value1));
                             }
-                            break;
+                            else
+                                dataRow.Values.Add("VALUE", new Data.DataValue(null));
 
-                        case "Set":
-                            for (int i = 0; i < serviceData[table].Values.Count; i++)
+                            table.DataRows.Add(dataRow);
+                        }
+                        break;
+
+                    case "Set":
+                        for (int i = 0; i < serviceData[cmdKey].Values.Count; i++)
+                        {
+                            var key = serviceData[cmdKey].GetValue("KEY", i);
+                            if (key != null && key is string str)
                             {
-                                var key = serviceData[table].GetValue("KEY", i);
-                                if (key != null)
-                                {
-                                    if (this.Hashtable.ContainsKey(key))
-                                        this.Hashtable[key] = serviceData[table].GetValue("VALUE", i);
-                                    else
-                                        this.Hashtable.Add(key, serviceData[table].GetValue("VALUE", i));
-                                }
+                                var value = (string?)serviceData[cmdKey].GetValue("VALUE", i);
+                                _memory.AddOrUpdate(str, value, (_, _) => value);
                             }
-                            break;
-                    }
+                        }
+                        break;
                 }
+            }
 
-                response.Status = Status.OK;
-            }
-            finally
-            {
-            }
+            response.Status = Status.OK;
 
             return response;
         }
